cbf5d72f304617532a65493f195e9bbb157c1dfa
Alex <sashais@bk.ru>
2022-12-28 15:10:49 +0500

    Solution task

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string [] arr = new string [6] {"yes", "ni", "754", "nene", "qwerty", "done" };
+string [] arr2 = new string [arr.Length];
+void FillArr (string[] arr)
+{
+    int j=0;
+    for (int i=0; i<arr.Length; i++)
+    {
+        //int j=0;
+        if (arr[i].Length<=3)
+        {
+            arr2[j] = arr[i];
+            j++;
+        }
+    }
+
+}
+
+void PrintArr(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+FillArr(arr);
+PrintArr(arr2);
